2ba434916b481b2e78453d596e53a5e708b06892
Alexander <80920304+KeFFiA@users.noreply.github.com>
2024-04-01 17:23:09 +0300

    Update path fetching errors

--- handlers.py
+++ handlers.py
@@ -1,37 +1,26 @@
+from tkinter.messagebox import showinfo, showerror
+
 from zarplata_script import identify
 from tkinter import filedialog
-import tkinter
-from config import root
 
 
 def apply_get():
-    global error, success
-    path = filedialog.askopenfilename(title='Поиск таблицы')
+    path = filedialog.askopenfilename()
     file_name1 = path.split(sep='.xlsx')
     file_name1[-1] = '(ГОТОВО)'
     file_name1.append('.xlsx')
     file_name1 = ''.join(file_name1)
-    print(file_name1)
-    print(path)
-    try:
-        success.destroy()
-        error.destroy()
-    except:
-        pass
+    file_name_success = path.split(sep='/')
+    file_name_success[-1] = ''
+    file_name_success = '/'.join(file_name_success)
     if path:
         try:
-            error.destroy()
-            success.destroy()
-        except:
-            pass
-        finally:
             identify(path, file_name1)
-            success = tkinter.Label(root, text='Успех!')
-            success.pack()
-    else:
-        try:
-            error = tkinter.Label(root, text='Файл не найден')
-            error.pack()
-            success.destroy()
+            message = f"Готовый файл находится в папке:\n{file_name_success}"
+            showinfo(title='Успех', message=message)
         except:
-            pass
+            message = f"Файл не найден или что-то пошло не так.\nПопробуй использовать другой файл."
+            showerror(title='Ошибка', message=message)
+    else:
+        message = f"Файл не найден или что-то пошло не так.\nПопробуй использовать другой файл."
+        showerror(title='Ошибка', message=message)
